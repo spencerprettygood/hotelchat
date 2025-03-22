7802b0e45cc66e94532cd17255ce8543f29b875b
patroni0901 <patroni0901@gmail.com>
2025-03-22 16:13:07 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -14,6 +14,7 @@ celery_app.conf.update(
     result_serializer='json',
     timezone='UTC',
     enable_utc=True,
+    broker_connection_retry_on_startup=True  # Add this to suppress the warning
 )
 
 @celery_app.task
